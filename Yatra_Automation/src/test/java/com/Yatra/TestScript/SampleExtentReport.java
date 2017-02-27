bf67ebfd41bcd3f3dde50183d62d2a0e40dd6032
narayana.reddy <narayana.reddy@aspire1282.Aspiresys.com>
2017-02-27 20:10:48 +0530

    Modifid testcase with Page object class

--- Yatra_Automation/src/test/java/com/Yatra/TestScript/SampleExtentReport.java
+++ Yatra_Automation/src/test/java/com/Yatra/TestScript/SampleExtentReport.java
@@ -2,14 +2,13 @@ package com.Yatra.TestScript;
 
 import java.util.HashMap;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.testng.ITestContext;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 
+import com.Yatra.Pages.HomePage;
 import com.Yatra.Utils.DataProviderUtils;
 import com.Yatra.Utils.EmailReport;
 import com.Yatra.Utils.EnvironmentPropertiesReader;
@@ -32,7 +31,8 @@ public class SampleExtentReport {
 				: context.getCurrentXmlTest().getParameter("webSite"));
 	}
 
-	@Test(groups = { "desktop" }, description = "To verify the Yatra Home Page", dataProviderClass = DataProviderUtils.class, dataProvider = "parallelTestDataProvider")
+	@Test(groups = {
+			"desktop" }, description = "To verify the Yatra Home Page", dataProviderClass = DataProviderUtils.class, dataProvider = "parallelTestDataProvider")
 	public void TC_Yatra_001(String browser) throws Exception {
 
 		HashMap<String, String> testData = TestDataExtractor.initTestData(workbookName, sheetName);
@@ -44,32 +44,28 @@ public class SampleExtentReport {
 		// Get the web driver instance
 		final WebDriver driver = WebDriverFactory.get(browser);
 		Log.testCaseInfo(testData);
+		try {
+			// step1: Navigate to Yatra Home Page
+			HomePage homePage = new HomePage(driver, webSite).get();
+			Log.message("1. Navigated to 'Yatra' Home Page!");
 
-		try {			
-			driver.get(webSite);
-			Thread.sleep(6000);
-		  
-			// step1: verify Yatra title bar text
+			// step2: verify Yatra title bar text
 			if (driver.getTitle().contains("Flight")) {
-				Log.message("1.Verified Yatra Title text");
+				Log.message("2.Verified Yatra Title text");
 			}
 
-			// step2: enter Origin place in Yatra Home page
-			WebElement selectOrgion = driver.findElement(By.id("BE_flight_origin_city"));
-			selectOrgion.sendKeys(origin);
-			Log.message("2.Successfully entered Origin Place in Yatra Homepage: " + origin);
+			// step3: enter Origin place in Yatra Home page
+			homePage.enterOrgion(origin);
+			Log.message("3.Successfully entered Origin Place in Yatra Homepage: " + origin);
+
+			// step4: enter Destination place in Yatra Home page
+			homePage.enterDestination(destination);
+			Log.message("4.Successfully entered Destination Place in Yatra Homepage: " + destination);
 			Thread.sleep(3000);
-			
-			// step3: enter Destination place in Yatra Home page
-			WebElement selectDestination = driver.findElement(By.id("BE_flight_arrival_city"));
-			selectDestination.sendKeys(destination);
-			Log.message("3.Successfully entered Destination Place in Yatra Homepage: " + destination);
-			Thread.sleep(3000);
-			
-			// step4: click 'Search' button in Yatra Home page
-			WebElement searchBtn = driver.findElement(By.id("BE_flight_flsearch_btn"));
-			searchBtn.click();
-			Log.message("4.Successfully clicked 'Search' in Yatra Homepage ");
+
+			// step5: click 'Search' button in Yatra Home page
+			homePage.clickBtnSearch();
+			Log.message("5.Successfully clicked 'Search' in Yatra Homepage ");
 
 			Log.message("<b>Expected Result:</b> Successfully searched Flights");
 
