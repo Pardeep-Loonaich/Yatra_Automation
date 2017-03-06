e77405c9079fedd2a9ae91a92c3e463eb0b57f62
nitika.katiyar <nitika.katiyar@aspireggn87.Aspiresys.com>
2017-03-06 14:59:54 +0530

    conflict files commit

---
+++ Yatra_Automation/src/test/java/com/Yatra/Pages/AgentRegister.java
@@ -0,0 +1,52 @@
+package com.Yatra.Pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.pagefactory.AjaxElementLocatorFactory;
+import org.openqa.selenium.support.pagefactory.ElementLocatorFactory;
+import org.openqa.selenium.support.ui.LoadableComponent;
+import org.testng.Assert;
+
+import com.Yatra.Utils.Log;
+import com.Yatra.Utils.Utils;
+
+public class AgentRegister extends LoadableComponent<AgentRegister> {
+
+	private boolean isPageLoaded;
+	private WebDriver driver;
+	
+	/**********************************************************************************************
+	 ********************************* WebElements of Yatra Home Page ***********************************
+	 **********************************************************************************************/
+	
+	@FindBy(id = "submitbutton")
+	public WebElement submitBtn;
+	
+	/**********************************************************************************************
+	 ********************************* WebElements of Home Page - Ends ****************************
+	 **********************************************************************************************/
+	@Override
+	protected void isLoaded() throws Error {
+		if (!isPageLoaded) {
+			Assert.fail();
+		}
+		if (isPageLoaded && !(Utils.waitForElement(driver, submitBtn))) {
+			Log.fail("Agent Login Page did not open up. Site might be down.", driver);
+		}	
+	}
+
+	@Override
+	protected void load() {
+		Utils.waitForPageLoad(driver);
+		isPageLoaded = true;	
+	}
+	
+	public AgentRegister(WebDriver driver) {
+		this.driver = driver;
+		ElementLocatorFactory finder = new AjaxElementLocatorFactory(driver,
+				Utils.maxElementWait);
+		PageFactory.initElements(finder, this);
+	}
+}
