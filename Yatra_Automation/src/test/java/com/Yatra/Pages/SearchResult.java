6916498a5b23cb2cba692634c3284cbeeacbb768
nitika.katiyar <nitika.katiyar@aspireggn87.Aspiresys.com>
2017-03-09 17:38:07 +0530

    updated search result

---
+++ Yatra_Automation/src/test/java/com/Yatra/Pages/SearchResult.java
@@ -0,0 +1,67 @@
+
+package com.Yatra.Pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.pagefactory.AjaxElementLocatorFactory;
+import org.openqa.selenium.support.pagefactory.ElementLocatorFactory;
+import org.openqa.selenium.support.ui.LoadableComponent;
+import org.testng.Assert;
+
+import com.Yatra.Utils.BrowserActions;
+import com.Yatra.Utils.Log;
+import com.Yatra.Utils.Utils;
+
+
+public class SearchResult extends LoadableComponent<SearchResult>{
+	
+	private WebDriver driver;
+	private boolean isPageLoaded;
+	
+	/**********************************************************************************************
+	 ********************************* WebElements of Yatra Home Page ***********************************
+	 **********************************************************************************************/
+	@FindBy(xpath = "//*[@title='Modify Search']")
+	public WebElement BtnModifySearch;
+	
+	/**********************************************************************************************
+	 ********************************* WebElements of Home Page - Ends ****************************
+	 **********************************************************************************************/
+	
+	/**
+	 * constructor of the class
+	 * 
+	 * @param driver
+	 *            : WebDriver
+	 * 
+	 * @param url
+	 *            : UAT URL
+	 */
+	
+	public SearchResult(WebDriver driver) {
+		this.driver = driver;
+		ElementLocatorFactory finder = new AjaxElementLocatorFactory(driver, Utils.maxElementWait);
+		PageFactory.initElements(finder, this);
+	}
+	
+	@Override
+	protected void isLoaded() {
+
+		if (!isPageLoaded) {
+			Assert.fail();
+		}
+
+		if (isPageLoaded && !(Utils.waitForElement(driver, BtnModifySearch))) {
+			Log.fail("Search Result page didn't open up", driver);
+		}
+	}
+
+	@Override
+	protected void load() {
+		isPageLoaded = true;
+		Utils.waitForPageLoad(driver);
+	}
+	
+}
