ef9b1482bf246d915b21835df5d13ced907cf9b9
nitika.katiyar <nitika.katiyar@aspireggn87.Aspiresys.com>
2017-03-17 12:05:59 +0530

    removed extra comments

--- Yatra_Automation/src/test/java/com/Yatra/Pages/HomePage.java
+++ Yatra_Automation/src/test/java/com/Yatra/Pages/HomePage.java
@@ -540,9 +540,7 @@ public class HomePage extends LoadableComponent<HomePage> {
 		selectDepartureDate(departureDate); // select Departure Date
 		specifyPassengerInfo(passengerInfo); // select Passengers details(Adult, Child, Infant)
 		selectPassengerClass(passengerClass); // select Passengers class type
-
-		clickDoneButtonInPassengerBox(); // click Done button
-		//specifyPassengerInfo(passengerInfo); // select Passengers with class	
+		clickDoneButtonInPassengerBox(); // click Done button	
         Log.event("Successfully selected OneWay Flight Search fields");
 
 	}
@@ -551,7 +549,6 @@ public class HomePage extends LoadableComponent<HomePage> {
 	 * To select Round Trip Flight search Fields
 	 * 
 	 * @throws Exception
-<<<<<<< HEAD
 	 */
 	
 
@@ -563,10 +560,7 @@ public class HomePage extends LoadableComponent<HomePage> {
 		selectReturnDate(returnDate); // select Return Date
 		specifyPassengerInfo(passengerInfo); // select Passengers details (Adult, Child, Infant)
 		selectPassengerClass(passengerClass); // select Passengers class type
-
 		clickDoneButtonInPassengerBox(); // click Done button
-		specifyPassengerInfo(passengerInfo); // select Passengers with class
-
 		Log.event("Successfully selected RoundTrip Flight Search fields");
 	}
 	
