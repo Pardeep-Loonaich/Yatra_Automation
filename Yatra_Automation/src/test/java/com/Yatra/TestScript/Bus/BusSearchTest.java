3b573fd02d548bbd13d11377b6061168e6848e87
vandna.uppal <vandna.uppal@aspireggn55.Aspiresys.com>
2017-04-06 10:25:34 +0530

    Updated Bus and Payment xls and xml.

--- Yatra_Automation/src/test/java/com/Yatra/TestScript/Bus/BusSearchTest.java
+++ Yatra_Automation/src/test/java/com/Yatra/TestScript/Bus/BusSearchTest.java
@@ -12,6 +12,7 @@ import java.util.Arrays;
 
 import java.util.HashMap;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.testng.ITestContext;
 import org.testng.annotations.BeforeTest;
@@ -44,7 +45,7 @@ public class BusSearchTest {
 	}
 
 	@Test(groups = {
-			"desktop" }, description = "Search Oneway bus for 1 pax", dataProviderClass = DataProviderUtils.class, dataProvider = "multipleExecutionData")
+	"desktop" }, description = "Search Oneway bus for 1 pax", dataProviderClass = DataProviderUtils.class, dataProvider = "multipleExecutionData")
 	public void TC_Yatra_Bus_001(HashMap<String, String> testData) throws Exception {
 
 		String browser = testData.get("browser");
@@ -83,7 +84,7 @@ public class BusSearchTest {
 							searchResultBus),
 					"<b>Actual Result:</b> User Successfully navigated on SearchResult page and Bus Details are as : "
 							+ BusDetail,
-					"<b>Actual Result:</b> User is not navigated on SearchResult page", driver);
+							"<b>Actual Result:</b> User is not navigated on SearchResult page", driver);
 
 			Log.testCaseResult();
 
@@ -96,7 +97,7 @@ public class BusSearchTest {
 	}
 
 	@Test(groups = {
-			"desktop" }, description = "Search Oneway bus for multiple pax", dataProviderClass = DataProviderUtils.class, dataProvider = "multipleExecutionData")
+	"desktop" }, description = "Search Oneway bus for multiple pax", dataProviderClass = DataProviderUtils.class, dataProvider = "multipleExecutionData")
 	public void TC_Yatra_Bus_002(HashMap<String, String> testData) throws Exception {
 
 		String browser = testData.get("browser");
@@ -135,7 +136,7 @@ public class BusSearchTest {
 							searchResultBus),
 					"<b>Actual Result:</b> User Successfully navigated on SearchResult page and Bus Details are as : "
 							+ BusDetail,
-					"<b>Actual Result:</b> User is not navigated on SearchResult page", driver);
+							"<b>Actual Result:</b> User is not navigated on SearchResult page", driver);
 
 			Log.testCaseResult();
 
@@ -148,7 +149,7 @@ public class BusSearchTest {
 	}
 
 	@Test(groups = {
-			"desktop" }, description = "Search RoundTrip bus for 1 pax", dataProviderClass = DataProviderUtils.class, dataProvider = "multipleExecutionData")
+	"desktop" }, description = "Search RoundTrip bus for 1 pax", dataProviderClass = DataProviderUtils.class, dataProvider = "multipleExecutionData")
 	public void TC_Yatra_Bus_003(HashMap<String, String> testData) throws Exception {
 
 		String browser = testData.get("browser");
@@ -199,7 +200,7 @@ public class BusSearchTest {
 	}
 
 	@Test(groups = {
-			"desktop" }, description = "Search RoundTrip bus for multiple pax", dataProviderClass = DataProviderUtils.class, dataProvider = "multipleExecutionData")
+	"desktop" }, description = "Search RoundTrip bus for multiple pax", dataProviderClass = DataProviderUtils.class, dataProvider = "multipleExecutionData")
 	public void TC_Yatra_Bus_004(HashMap<String, String> testData) throws Exception {
 
 		String browser = testData.get("browser");
@@ -250,7 +251,7 @@ public class BusSearchTest {
 	}
 
 	@Test(groups = {
-			"desktop" }, description = "Don't select any city for onward/return", dataProviderClass = DataProviderUtils.class, dataProvider = "multipleExecutionData")
+	"desktop" }, description = "Don't select any city for onward/return", dataProviderClass = DataProviderUtils.class, dataProvider = "multipleExecutionData")
 	public void TC_Yatra_Bus_005(HashMap<String, String> testData) throws Exception {
 
 		String browser = testData.get("browser");
@@ -290,7 +291,7 @@ public class BusSearchTest {
 			Log.assertThat(homePage.elementLayer.verifyPageElements(Arrays.asList("btnSearchBus"), homePage),
 					"<b>Actual Result:</b> After Clicking Search Button, An Error Message is Displayed as :"
 							+ ErrorMessage,
-					"<b>Actual Result:</b> After Clicking Search Button, No Error Message is not Displayed", driver);
+							"<b>Actual Result:</b> After Clicking Search Button, No Error Message is not Displayed", driver);
 
 			Log.testCaseResult();
 
@@ -303,7 +304,7 @@ public class BusSearchTest {
 	}
 
 	@Test(groups = {
-			"desktop" }, description = "Don't select any date to travel", dataProviderClass = DataProviderUtils.class, dataProvider = "multipleExecutionData")
+	"desktop" }, description = "Don't select any date to travel", dataProviderClass = DataProviderUtils.class, dataProvider = "multipleExecutionData")
 	public void TC_Yatra_Bus_006(HashMap<String, String> testData) throws Exception {
 
 		String browser = testData.get("browser");
@@ -347,7 +348,7 @@ public class BusSearchTest {
 			Log.assertThat(homePage.elementLayer.verifyPageElements(Arrays.asList("btnSearchBus"), homePage),
 					"<b>Actual Result:</b> After Clicking Search Button, An Error Message is Displayed as :"
 							+ ErrorMessage,
-					"<b>Actual Result:</b> After Clicking Search Button, No Error Message is not Displayed", driver);
+							"<b>Actual Result:</b> After Clicking Search Button, No Error Message is not Displayed", driver);
 
 			Log.testCaseResult();
 
@@ -360,7 +361,7 @@ public class BusSearchTest {
 	}
 
 	@Test(groups = {
-			"desktop" }, description = "On selecting same city for depart & Arrival", dataProviderClass = DataProviderUtils.class, dataProvider = "multipleExecutionData")
+	"desktop" }, description = "On selecting same city for depart & Arrival", dataProviderClass = DataProviderUtils.class, dataProvider = "multipleExecutionData")
 	public void TC_Yatra_Bus_007(HashMap<String, String> testData) throws Exception {
 
 		String browser = testData.get("browser");
@@ -400,7 +401,7 @@ public class BusSearchTest {
 			Log.assertThat(homePage.elementLayer.verifyPageElements(Arrays.asList("btnSearchBus"), homePage),
 					"<b>Actual Result:</b> After Clicking Search Button, An Error Message is Displayed as :"
 							+ ErrorMessage,
-					"<b>Actual Result:</b> After Clicking Search Button, No Error Message is not Displayed", driver);
+							"<b>Actual Result:</b> After Clicking Search Button, No Error Message is not Displayed", driver);
 
 			Log.testCaseResult();
 
@@ -413,7 +414,7 @@ public class BusSearchTest {
 	}
 
 	@Test(groups = {
-			"desktop" }, description = "Enter incorrectCity Name for onward or return", dataProviderClass = DataProviderUtils.class, dataProvider = "multipleExecutionData")
+	"desktop" }, description = "Enter incorrectCity Name for onward or return", dataProviderClass = DataProviderUtils.class, dataProvider = "multipleExecutionData")
 	public void TC_Yatra_Bus_008(HashMap<String, String> testData) throws Exception {
 
 		String browser = testData.get("browser");
@@ -453,7 +454,7 @@ public class BusSearchTest {
 			Log.assertThat(homePage.elementLayer.verifyPageElements(Arrays.asList("btnSearchBus"), homePage),
 					"<b>Actual Result:</b> After Clicking Search Button, An Error Message is Displayed as :"
 							+ ErrorMessage,
-					"<b>Actual Result:</b> After Clicking Search Button, No Error Message is not Displayed", driver);
+							"<b>Actual Result:</b> After Clicking Search Button, No Error Message is not Displayed", driver);
 
 			Log.testCaseResult();
 
@@ -466,7 +467,7 @@ public class BusSearchTest {
 	}
 
 	@Test(groups = {
-			"desktop" }, description = "Check for Message in Case Of No Result", dataProviderClass = DataProviderUtils.class, dataProvider = "multipleExecutionData")
+	"desktop" }, description = "Check for Message in Case Of No Result", dataProviderClass = DataProviderUtils.class, dataProvider = "multipleExecutionData")
 	public void TC_Yatra_Bus_009(HashMap<String, String> testData) throws Exception {
 
 		String browser = testData.get("browser");
@@ -506,8 +507,8 @@ public class BusSearchTest {
 							searchResultBus),
 					"<b>Actual Result:</b> User Successfully navigated on SearchResult page and On SRP details as  : "
 							+ SRPDetail,
-					"<b>Actual Result:</b> User navigated on SearchResult Page,NO Bus Found Message not displayed On SRP Page",
-					driver);
+							"<b>Actual Result:</b> User navigated on SearchResult Page,NO Bus Found Message not displayed On SRP Page",
+							driver);
 
 			Log.testCaseResult();
 
@@ -520,7 +521,7 @@ public class BusSearchTest {
 	}
 
 	@Test(groups = {
-			"desktop" }, description = "User Should See the Price in Sorted Form in Ascending Order", dataProviderClass = DataProviderUtils.class, dataProvider = "multipleExecutionData")
+	"desktop" }, description = "User Should See the Price in Sorted Form in Ascending Order", dataProviderClass = DataProviderUtils.class, dataProvider = "multipleExecutionData")
 	public void TC_Yatra_Bus_010(HashMap<String, String> testData) throws Exception {
 
 		String browser = testData.get("browser");
@@ -569,7 +570,7 @@ public class BusSearchTest {
 	}
 
 	@Test(groups = {
-			"desktop" }, description = "User Should See the depart time in Sorted Form", dataProviderClass = DataProviderUtils.class, dataProvider = "multipleExecutionData")
+	"desktop" }, description = "User Should See the depart time in Sorted Form", dataProviderClass = DataProviderUtils.class, dataProvider = "multipleExecutionData")
 	public void TC_Yatra_Bus_011(HashMap<String, String> testData) throws Exception {
 
 		String browser = testData.get("browser");
@@ -623,7 +624,7 @@ public class BusSearchTest {
 	}
 
 	@Test(groups = {
-			"desktop" }, description = "User Should See the Arrive time in Sorted Form", dataProviderClass = DataProviderUtils.class, dataProvider = "multipleExecutionData")
+	"desktop" }, description = "User Should See the Arrive time in Sorted Form", dataProviderClass = DataProviderUtils.class, dataProvider = "multipleExecutionData")
 	public void TC_Yatra_Bus_012(HashMap<String, String> testData) throws Exception {
 
 		String browser = testData.get("browser");
@@ -677,7 +678,7 @@ public class BusSearchTest {
 	}
 
 	@Test(groups = {
-			"desktop" }, description = "User Should See the Duration in Sorted Form", dataProviderClass = DataProviderUtils.class, dataProvider = "multipleExecutionData")
+	"desktop" }, description = "User Should See the Duration in Sorted Form", dataProviderClass = DataProviderUtils.class, dataProvider = "multipleExecutionData")
 	public void TC_Yatra_Bus_013(HashMap<String, String> testData) throws Exception {
 
 		String browser = testData.get("browser");
@@ -730,7 +731,7 @@ public class BusSearchTest {
 	}
 
 	@Test(groups = {
-			"desktop" }, description = "On select seats should open seat-map page", dataProviderClass = DataProviderUtils.class, dataProvider = "multipleExecutionData")
+	"desktop" }, description = "On select seats should open seat-map page", dataProviderClass = DataProviderUtils.class, dataProvider = "multipleExecutionData")
 	public void TC_Yatra_Bus_014(HashMap<String, String> testData) throws Exception {
 
 		String browser = testData.get("browser");
@@ -760,7 +761,7 @@ public class BusSearchTest {
 			searchResultBus = homePage.clickBtnSearchBus();
 			Log.message("5. Clicked On Search Button!");
 
-			searchResultBus.clickBtnSelectSeat();
+			searchResultBus.clickBtnSelectSeat(1);
 			Log.message("6. Clicked On Select Seat!");
 
 			Log.message("<br>");
@@ -782,7 +783,7 @@ public class BusSearchTest {
 	}
 
 	@Test(groups = {
-			"desktop" }, description = "Click on bus details link", dataProviderClass = DataProviderUtils.class, dataProvider = "multipleExecutionData")
+	"desktop" }, description = "Click on bus details link", dataProviderClass = DataProviderUtils.class, dataProvider = "multipleExecutionData")
 	public void TC_Yatra_Bus_015(HashMap<String, String> testData) throws Exception {
 
 		String browser = testData.get("browser");
@@ -834,7 +835,7 @@ public class BusSearchTest {
 	}
 
 	@Test(groups = {
-			"desktop" }, description = "Check for Prev/Next day tabs", dataProviderClass = DataProviderUtils.class, dataProvider = "multipleExecutionData")
+	"desktop" }, description = "Check for Prev/Next day tabs", dataProviderClass = DataProviderUtils.class, dataProvider = "multipleExecutionData")
 	public void TC_Yatra_Bus_016(HashMap<String, String> testData) throws Exception {
 
 		String browser = testData.get("browser");
@@ -864,7 +865,7 @@ public class BusSearchTest {
 			searchResultBus = homePage.clickBtnSearchBus();
 			Log.message("5. Clicked On Search Button!");
 
-			searchResultBus.clickBtnSelectSeat();
+			searchResultBus.clickBtnSelectSeat(1);
 			Log.message("6. Clicked On Select Seat!");
 			Thread.sleep(6000);
 
@@ -889,7 +890,7 @@ public class BusSearchTest {
 	}
 
 	@Test(groups = {
-			"desktop" }, description = "Check for RT /OW dates", dataProviderClass = DataProviderUtils.class, dataProvider = "multipleExecutionData")
+	"desktop" }, description = "Check for RT /OW dates", dataProviderClass = DataProviderUtils.class, dataProvider = "multipleExecutionData")
 	public void TC_Yatra_Bus_017(HashMap<String, String> testData) throws Exception {
 
 		String browser = testData.get("browser");
@@ -942,7 +943,7 @@ public class BusSearchTest {
 	}
 
 	@Test(groups = {
-			"desktop" }, description = "Check for validations in city names", dataProviderClass = DataProviderUtils.class, dataProvider = "multipleExecutionData")
+	"desktop" }, description = "Check for validations in city names", dataProviderClass = DataProviderUtils.class, dataProvider = "multipleExecutionData")
 	public void TC_Yatra_Bus_018(HashMap<String, String> testData) throws Exception {
 
 		String browser = testData.get("browser");
@@ -986,8 +987,8 @@ public class BusSearchTest {
 							searchResultBus),
 					"<b>Actual Result:</b> User navigated to Search Result Page, and Error Message is Displayed as :"
 							+ error,
-					"<b>Actual Result:</b> User navigated to Search Result Page, But No Error Message is Displayed",
-					driver);
+							"<b>Actual Result:</b> User navigated to Search Result Page, But No Error Message is Displayed",
+							driver);
 
 			Log.testCaseResult();
 
@@ -1000,7 +1001,7 @@ public class BusSearchTest {
 	}
 
 	@Test(groups = {
-			"desktop" }, description = "Check for Dropping points, Boarding point, Bus type, Amenities, Price, depart time, arrival time", dataProviderClass = DataProviderUtils.class, dataProvider = "multipleExecutionData")
+	"desktop" }, description = "Check for Dropping points, Boarding point, Bus type, Amenities, Price, depart time, arrival time", dataProviderClass = DataProviderUtils.class, dataProvider = "multipleExecutionData")
 	public void TC_Yatra_Bus_019(HashMap<String, String> testData) throws Exception {
 
 		String browser = testData.get("browser");
@@ -1031,7 +1032,7 @@ public class BusSearchTest {
 			Log.message("5. Clicked On Search Button!");
 			Thread.sleep(6000);
 
-			searchResultBus.clickBtnSelectSeat();
+			searchResultBus.clickBtnSelectSeat(1);
 			Log.message("6. Clicked On Select Seat!");
 			Thread.sleep(6000);
 			String details = searchResultBus.getTextBusInfo();
@@ -1055,7 +1056,7 @@ public class BusSearchTest {
 					"<b>Actual Result:</b> User navigated to Search Result Page,All details are properly seen as -->"
 							+ details + "Boarding Point as --> " + BoardingPoint + "Dropping Point As -->"
 							+ DroppingPoint,
-					"<b>Actual Result:</b> ", driver);
+							"<b>Actual Result:</b> ", driver);
 
 			Log.testCaseResult();
 
@@ -1068,7 +1069,7 @@ public class BusSearchTest {
 	}
 
 	@Test(groups = {
-			"desktop" }, description = "Check for correct seat number and type shown", dataProviderClass = DataProviderUtils.class, dataProvider = "multipleExecutionData")
+	"desktop" }, description = "Check for correct seat number and type shown", dataProviderClass = DataProviderUtils.class, dataProvider = "multipleExecutionData")
 	public void TC_Yatra_Bus_020(HashMap<String, String> testData) throws Exception {
 
 		String browser = testData.get("browser");
@@ -1098,9 +1099,12 @@ public class BusSearchTest {
 			searchResultBus = homePage.clickBtnSearchBus();
 			Log.message("5. Clicked On Search Button!");
 
-			searchResultBus.clickBtnSelectSeat();
+			searchResultBus.clickBtnSelectSeat(3);
 			Log.message("6. Clicked On Select Seat!");
-
+			/*driver.manage().window().maximize();
+			driver.switchTo().frame(driver.findElement(By.tagName("iframe"))); // switching the frame by ID
+			 */
+			searchResultBus.switchToiframe();
 			searchResultBus.selectSeat(2);
 			Log.message("7. Seat Selected!");
 			String SeatNumber = searchResultBus.getTextSeatNumber();
