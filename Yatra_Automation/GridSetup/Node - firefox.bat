dd6a0a3ed6e802f98f218af34497d2712877f9bd
vandna.uppal <vandna.uppal@aspireggn55.Aspiresys.com>
2017-05-26 16:11:18 +0530

    Added new GridSetup

--- Yatra_Automation/GridSetup/Node - firefox.bat
+++ Yatra_Automation/GridSetup/Node - firefox.bat
@@ -1,6 +1,7 @@
+SET title=FirefoxNode
 SET HUBHOST=localhost
 SET NODEHOST=localhost
 IF NOT [%1] == [] SET HUBHOST=%1
 IF NOT [%1] == [] SET NODEHOST=%1
-start java -Dwebdriver.gecko.driver=geckodriver.exe -jar selenium-server-standalone-3.3.1.jar -role webdriver -hub http://%HUBHOST%:4444/grid/register -hubHost %HUBHOST% -host %NODEHOST% -port 5555 -browser "browserName=firefox, maxInstances=10"
+start java -Dwebdriver.gecko.driver=geckodriver.exe -jar selenium-server-standalone-3.4.0.jar -role webdriver -hub http://%HUBHOST%:4444/grid/register -hubHost %HUBHOST% -host %NODEHOST% -port 5555 -browser "browserName=firefox, maxInstances=10"
 Exit
