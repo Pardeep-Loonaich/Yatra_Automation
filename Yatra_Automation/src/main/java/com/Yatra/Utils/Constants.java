950d9ddd61e59ceeee59cbea4b33c90842e8853e
harveer.singh <harveer.singh@aspiresys.com>
2017-06-23 14:08:23 +0530

    updated load time for bus

--- Yatra_Automation/src/main/java/com/Yatra/Utils/Constants.java
+++ Yatra_Automation/src/main/java/com/Yatra/Utils/Constants.java
@@ -6,14 +6,20 @@ import java.util.List;
 import java.util.Map;
 
 public class Constants {
+	
+	/**
+	 Page load time:
+	 */
+	public static final int Home_Page_Load_Time=10;
+	public static final int Search_Result_Page_Load_Time=30;
+	public static final int Bus_Search_Result_Page_Load_Time=30;
 
 	// >>> Flights Trip type
 	public static final String C_ONEWAY = "ONEWAY";
 	public static final String C_ROUNDTRIP = "ROUNDTRIP";
 	public static final String C_MULTICITY = "MULTICITY";
 	public static final String C_EXPLORE = "EXPLORE";
-	public static final int Home_Page_Load_Time=10;
-	public static final int Search_Result_Page_Load_Time=30;
+	
 	
 	// '>>> Flights Passengers class types
 	public static final String C_ECONOMY = "Economy";
